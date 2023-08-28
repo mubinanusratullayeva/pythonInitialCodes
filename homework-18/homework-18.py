63008f40864efb4e0c22253299096bc09b2828a8
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-08-28 06:09:55 -0700

    last homework commit

---
+++ homework-18/homework-18.py
@@ -0,0 +1,9 @@
+num = 0
+
+while num < 10:
+    num += 1
+
+    if (num % 2) == 0:
+        continue
+
+    print(num)
---
+++ homework-20/homework-20.py
@@ -0,0 +1,6 @@
+languages = ["Python", "Swift", "C++"]
+
+print(languages[0])
+print(languages[2])
+print(languages[-1])
+print(languages[-3])
