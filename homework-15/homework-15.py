2dcd959fd97cf079ebc495143b2242e387283974
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-08-28 06:02:54 -0700

    python next homework

---
+++ homework-15/homework-15.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+thislist.append('aprcot')
+print(len(thislist))
+
+thislist.remove('apple')
+print(len(thislist))
---
+++ homework-8/homework-8.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3,4,5,6,6,7,8,9]
+
+print(numbers)
+
+numbers.append(34)
+print(numbers)
+
+numbers.remove(3)
+print(numbers)
