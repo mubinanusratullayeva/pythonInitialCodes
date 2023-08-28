4a9ffcc25456c6cf8f77d11799354fd2fd673626
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-08-28 06:07:16 -0700

    next homework from last

---
+++ homework-10/homework-10.py
@@ -0,0 +1,8 @@
+numbers_Backets = [1,2,3,4,5,7,8,9]
+print(numbers_Backets)
+
+numbF = numbers_Backets[4] * numbers_Backets[6]
+print(numbF)
+
+numbS = numbers_Backets[4] * 100 / numbers_Backets[1]
+print(numbS)
---
+++ homework-9/homework-9.py
@@ -0,0 +1,10 @@
+num = [1,2,3,4,5,6,7,8]
+print(num)
+
+# del is the short method for delete
+del num[1]
+print(num)
+
+# this method delete all numbers after index 5
+del num[5:]
+print(num)
